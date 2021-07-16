46b7d2f4968a8f39da1d3409d6b1718fb80af645
Damir Temir <dtemi2@uis.edu>
2021-07-16 13:01:30 -0700

    added the range specificity to resfile utility
    now, when you specify the range of residue ids (30 - 35), the utility will handle it appropriately.
    
    modified resfile_util.py to check for the key "-" within the arguments
    modified resfile.txt to test the function

--- seq_des/util/resfile_util.py
+++ seq_des/util/resfile_util.py
@@ -13,10 +13,14 @@ def read_resfile(filename):
     65 ALLAA # allow all amino acids at residue id 65 (64 in the tensor)
     54 ALLAAxc # allow all amino acids except cysteine at residue id 54 (53 in the tensor)
     30 POLAR # allow only polar amino acids at residue id 30 (29 in the tensor)
+    31 - 33 NOTAA CFYG # disallow the specified amino acids at residue ids 31 to 33 (30 to 32 in the tensor) 
 
-    results into a dictionary 
-    {64: {}, 53: {'C'}, 29: {'T', 'R', 'K', 'Q', 'D', 'E', 'S', 'N', 'H'}},
-    plus it returns a header from check_for_header()
+    results into a dictionary: 
+    {64: {}, 53: {'C'}, 29: {'T', 'R', 'K', 'Q', 'D', 'E', 'S', 'N', 'H'},
+    30: {'C', 'F', 'Y', 'G'}, 31: {'C', 'F', 'Y', 'G'}, 32: {'C', 'F', 'Y', 'G'}}
+
+    plus it returns a header from check_for_header():
+    {"DEFAULT": {}}
     """
     constraints = dict()
     header, start_id = check_for_header(filename)
@@ -29,7 +33,13 @@ def read_resfile(filename):
             assert isinstance(args[1], str), "the resfile command needs to be a string"
             
             res_id = int(args[0]) - 1
-            constraints[res_id] = check_for_commands(args)
+            if args[1] == "-": # if given a range of residue ids (ex. 31 - 33 NOTAA)
+                for res_id in range(res_id, int(args[2])):
+                    constraints[res_id] = check_for_commands(args, 3, 4)
+            else:
+                constraints[res_id] = check_for_commands(args, 1, 2)
+
+        print(constraints)
 
     return constraints, header
 
@@ -59,24 +69,27 @@ def check_for_header(filename):
                     break
                 args = line.split()
                 args.insert(0, "") # check_for_commands only handles the second argument (first is usually res_id)
-                header['DEFAULT'] = check_for_commands(args)
+                header['DEFAULT'] = check_for_commands(args, 1, 2)
 
     return header, start_id
 
 
-def check_for_commands(args):
+def check_for_commands(args, command_id, list_id):
     """
     converts given commands into sets of amino acids to restrict in the logits
     
     so far, it handles these commands: ALLAA, ALLAAxc, POLAR, APOLAR, NOTAA, PIKAA
+
+    command_id - the index where the command is within the args
+    list_id - the index where the possible list of AA is within the args (only for NOTAA and PIKAA) 
     """
     constraint = set()
-    command = args[1].upper()
+    command = args[command_id].upper()
     if command in common.atoms.resfile_commands.keys():
         constraint = common.atoms.resfile_commands["ALLAAwc"] - common.atoms.resfile_commands[command]
     elif command == "PIKAA": # allow only the specified amino acids
-        constraint = common.atoms.resfile_commands["ALLAAwc"] - set(args[2].strip())
+        constraint = common.atoms.resfile_commands["ALLAAwc"] - set(args[list_id].strip())
     elif command == "NOTAA": # disallow only the specified amino acids
-        constraint = set(args[2].strip())
+        constraint = set(args[list_id].strip())
 
     return constraint
